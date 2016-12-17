5a7ff4dc378570ba7c5e57ede5941b9f505c3d54
SleekPanther <macrobyte2@gmail.com>
2016-12-17 14:34:29 -0500

    html cleanup

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Invert Selection</title>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
+	
 	<script>
 		//add property to all inputs, doesn't care if it already exists
 		function selectAll(nameToInvert){
@@ -52,36 +52,47 @@
 </head>
 <body>
 	<h1><a href='https://github.com/SleekPanther/checkbox-toggle-selection'>Checkbox Toggle Selection</a></h1>
-	<p>PHP demo of invert, select all & none methods for checkboxed radio buttons <br><a href='html.html'>view plain html version</a></p>
+	<p>PHP demo of invert, select all & none methods for checkboxes & radio buttons <br><a href='html.html'>View plain html version</a></p>
 
-	<?php 
-	echo "<pre>";
+	<?php
+	//display form data submitted to itself
+	//remove these lines when done testing
+	echo '<pre>$_POST Array form data (for debugging)'."<br>";
 	print_r($_POST);
 	echo "</pre>";
 	?>
 
-	<form method="post" action='php.php'> 
-		<p>Checkboxes</p>
-		<input type='checkbox' name='chkBoxes[]' value='1'>
-		<input type='checkbox' name='chkBoxes[]' value='2'>
-		<input type='checkbox' name='chkBoxes[]' value='3'>
-		<input type='checkbox' name='chkBoxes[]' value='4'>
+	<form method="post" action='index.php'>
+		<h3>Checkboxes</h3>
+		<?php
+		//use a loop to print checkboxes in the same group
+		//data for the "value" attribute could come from a database & iterate for as many records exist
+		//If the value needs to be different from the label, then create a separate array for labels, or use a 2D array hold both label & value
+		$valuesArray=array(1,2,3,4);
+		foreach($valuesArray as $value){
+			echo "\t\t<label for='chk".$value."'>".$value."</label>\n";
+			echo "\t\t<input type='checkbox' id='chk".$value."' name='chkBoxes[]' value='".$value."'>\n";
+		}
+		?>
 		
 		<br>
-		<input type="button" name="btnAll" onclick="selectAll('chkBoxes[]')" value="All">
+		<input type="button" name="btnAll" onclick="selectAll('chkBoxes[]')" value="Select All">
 		<input type="button" name="btnNone" onclick="selectNone('chkBoxes[]')" value="None">
 		<input type="button" name="btnInvert" onclick="invert('chkBoxes[]')" value="Invert">
 
+		
+		<br><br>
+		<h3>Radio Buttons</h3>
+		<input type='radio' name='radRadios' id='radRadiosYes' value='Yes'>
+		<label for='radRadiosYes'>Yes</label><br>
+		<input type='radio' name='radRadios' id='radRadiosNo' value='No'>
+		<label for='radRadiosNo'>No</label><br>
+		<input type='radio' name='radRadios' id='radRadiosMaybe' value='Maybe'>
+		<label for='radRadiosMaybe'>Maybe</label><br>
+		<input type="button" name="btnNoneRad" onclick="selectNone('radRadios')" value="Deselect">
 
-		<p>Radio Buttons</p>
-		<input type='radio' name='radRadios' value='1'>
-		<input type='radio' name='radRadios' value='2'>
-		<input type="button" name="btnNone" onclick="selectNone('radRadios')" value="Deselect">
-
-		<br>
-		<input type="submit" value='Submit' name='btnSubmit'>
+		<br><br><br>
+		<input type="submit" value='Submit Form' name='btnSubmit'>
 	</form>
-	<p>Click on this paragraph.</p>
-
 </body>
 </html>
